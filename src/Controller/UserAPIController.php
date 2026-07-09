0ce419bb6e9e5fa124b2231d358137a26118690c
Rino <fullstack86@gmail.com>
2026-07-09 16:41:25 +0800

    first commit

---
+++ src/Controller/UserAPIController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+use App\Model\User;
+use App\Repository\UserRepository;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/users')]
+class UserController extends AbstractController
+{
+
+    #[Route('', methods:['GET'])]
+    public function getCollection(UserRepository $userRepository): \Symfony\Component\HttpFoundation\JsonResponse
+    {
+
+        $users = $userRepository->findAll();
+        return $this->json($users);
+
+    }
+    #[Route('/{id<\d+>}')]
+   public function get(int $id, UserRepository $userRepository){
+
+        $user = $userRepository->find($id);
+
+        if(!$user) throw $this->createNotFoundException("User not found");
+
+        return $this->json($user);
+   }
+}
